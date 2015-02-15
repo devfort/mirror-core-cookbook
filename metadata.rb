f4c7907eb0fdf1882604ec22d7e8b062ba16254e
Steve Marshall <steve@nascentguruism.com>
2015-02-15 12:58:15 +0000

    Update to support Apache 2.4
    Apache 2.4 [changes/deprecates a number of
    features](https://httpd.apache.org/docs/2.4/new_features_2_4.html).
    This ensures upgrading to Apache 2.4 doesn't break hosting mirrored
    files.

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer        "Steve Marshall"
 maintainer_email  "steve@nascentguruism.com"
 license           "Apache 2.0"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1"
+version           "0.2"
 depends           "apache2"
